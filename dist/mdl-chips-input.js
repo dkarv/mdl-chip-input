5f6ddaf19fb0c998733d923f0d49e0ab90257969
david <d.krebs@testbirds.de>
2016-11-08 20:22:31 +0100

    don't require jquery, program in mdl style, improve design

--- dist/mdl-chips-input.js
+++ dist/mdl-chips-input.js
@@ -1,98 +1,164 @@
-$.fn.chips_input = function(options) {
-    var objs = { field: $(this) };
-    objs.field.addClass('mdl-chipfield');
-    objs.field.children().wrapAll('<div class="original"></div>');
-    objs.field.prepend('<div class="chips"></div>');
-    objs.chips = objs.field.find('.chips');
-    objs.input = objs.field.find('.mdl-textfield__input');
-    objs.target = objs.field.find('.mdl-chipfield__input');
-    if(options.search) {
-        objs.results = $('<ul class="results"></ul>').insertAfter(objs.input);
-    }
+(function() {
+    'use strict';
 
-    function insertChip(text) {
-        var html =
-            '<span class="mdl-chip mdl-chip--deletable">' +
+    var MaterialChipInput = function MaterialChipInput(element) {
+        this.element_ = element;
+
+        this.init();
+    };
+    window['MaterialChipInput'] = MaterialChipInput;
+
+
+    MaterialChipInput.prototype.Constant_ = {};
+    MaterialChipInput.prototype.CssClasses_ = {};
+
+    MaterialChipInput.prototype.setProgress = function(p) {
+
+    };
+
+    MaterialChipInput.prototype.addChip_ = function(text) {
+        var chip = document.createElement('span');
+        chip.classList = 'mdl-chip mdl-chip--deletable';
+        chip.innerHTML =
             '<span class="mdl-chip__text">' + text + '</span>' +
             '<button type="button" class="mdl-chip__action">' +
-            '<i class="material-icons">cancel</i></button></span>';
-        objs.chips.append(html);
-    }
+            '<i class="material-icons">close</i></button>';
+        var update = this.updateTarget_.bind(this);
+        chip.getElementsByClassName('mdl-chip__action')[0].onclick = function() {
+            console.log('removing', text);
+            chip.remove();
+            update();
+        };
+        this.element_.insertBefore(chip, this.inputs_);
+    };
 
-    function getChips() {
-        return objs.chips.children().map(function() {
-            return $(this).find('.mdl-chip__text').text();
-        }).get();
-    }
-
-    if(objs.target.val()) {
-        // initialize the chips
-        objs.target.val().split(',').forEach(insertChip);
-    }
-
-    objs.input.on('keydown', function(e) {
-        if(e.keyCode === 8 && !objs.input.val()) {
-            // remove last tag if input is empty
-            objs.chips.children().last().remove();
-        }
-        return e.keyCode !== 13;
-    });
-    objs.input.on('keyup', function(e) {
-        if([13, 32, 188].indexOf(e.keyCode) > -1) {
-            var content = objs.input.val().replace(/[^0-9a-zäüö]/gi, '');
-            if(content) {
-                objs.input.val('');
-                var currentChips = getChips();
-                if(currentChips.indexOf(content) === -1) {
-                    insertChip(content);
-                    currentChips.push(content);
-                    objs.target.val(currentChips.join(','));
-                    objs.field.removeClass('is-dirty');
-                }
+    MaterialChipInput.prototype.getChips = function() {
+        var currentChips = [];
+        var children = this.element_.children;
+        for(var i = children.length; i--;) {
+            if(children[i].classList.contains('mdl-chip')) {
+                currentChips.unshift(
+                    children[i].children[0].textContent ||
+                    children[i].children[0].innerText);
             }
+        }
+        return currentChips;
+    };
+
+    MaterialChipInput.prototype.updateTarget_ = function() {
+        var currentChips = this.getChips();
+        this.target_.value = currentChips.join(this.options_.separator);
+        if(currentChips.length >= this.options_.maximum) {
+            this.input_.style.display = 'none';
         } else {
-            if(options.search) {
-                var query = objs.input.val();
-                options.search(query, function(result) {
-                    console.log(result);
-                    objs.results.empty();
-                    result.forEach(function(res) {
-                        // highlight the search results
-                        objs.results.append("<li>" + res + "</li>");
-                    })
-                });
+            this.input_.style.display = 'block';
+        }
+    };
+
+    MaterialChipInput.prototype.startSearch_ = function() {
+        if(this.results_) {
+            var query = this.input_.value;
+            this.search(query, function(result) {
+                console.log(result);
+                this.results_.innerHTML = result.map(function(res) {
+                    return '<li>' + res + '</li>';
+                }).join('\n');
+            });
+        }
+    };
+
+    MaterialChipInput.prototype.clearResults_ = function() {
+        if(this.results_) {
+            for(var i = this.results_.children.length; i--;) {
+                this.results_.children[i].remove();
             }
         }
-        return false;
-    });
-    if(options.search) {
-        objs.results.on('click', 'li', function() {
-            var text = $(this).text();
-            if(getChips().indexOf(text) === -1) {
-                insertChip(text);
+    };
+
+    MaterialChipInput.prototype.mouseDown_ = function(event) {
+        this.input_.focus();
+    };
+
+    MaterialChipInput.prototype.keyDown_ = function(event) {
+        var code = event.which || event.keyCode;
+        if(code === 8 && !this.input_.value) {
+            // remove last tag if input is empty
+            if(this.element_.children.length > 1) {
+                this.element_.children[this.element_.children.length - 2].remove();
             }
-            objs.results.empty();
-            objs.input.val('');
-        });
+        }
+        if(code === 13) {
+            // prevent enter from submitting the form
+            event.preventDefault();
+        }
+    };
+
+    MaterialChipInput.prototype.keyUp_ = function(event) {
+        var code = event.which || event.keyCode;
+        // TODO use more reasonable logic here
+        if([13, 32, 188].indexOf(code) > -1) {
+            var content = this.input_.value.replace(/[^0-9a-zäüö]/gi, '');
+            if(content) {
+                var currentChips = this.getChips();
+                // ignore duplicates
+                if(currentChips.indexOf(content) === -1) {
+                    this.addChip_(content);
+                    this.updateTarget_();
+                    this.element_.classList.remove('is-dirty');
+                }
+
+                // delete search results
+                this.clearResults_();
+            }
+            this.input_.value = '';
+        } else {
+            this.startSearch_();
+        }
+        event.preventDefault();
+    };
+
+    MaterialChipInput.prototype.addSearch = function(search) {
+        this.results_ = document.createElement('ul');
+        this.results_.classList = 'results';
+        this.inputs_.insertBefore(this.results_, this.input_.nextSibling);
+        this.options_.search = search;
+
+        // TODO add onclick handler
+        /* objs.results.on('click', 'li', function() {
+         var text = $(this).text();
+         if(getChips().indexOf(text) === -1) {
+         insertChip(text);
+         }
+         objs.results.empty();
+         objs.input.val('');
+         });*/
+    };
+
+    MaterialChipInput.prototype.init = function() {
+        if(this.element_) {
+            // wrap all content in the .inputs div
+            this.element_.innerHTML = '<div class="inputs">' + this.element_.innerHTML + '</div>';
+            this.inputs_ = this.element_.children[0];
+
+            this.input_ = this.element_.getElementsByClassName('mdl-textfield__input')[0];
+            this.input_.addEventListener('keydown', this.keyDown_.bind(this));
+            this.input_.addEventListener('keyup', this.keyUp_.bind(this));
+            this.target_ = this.element_.getElementsByClassName('mdl-chipfield__input')[0];
+
+            // let the whole element look like a input field
+            this.element_.addEventListener('click', this.mouseDown_.bind(this));
+
+            // set the default options
+            this.options_ = {
+                maximum: Number.MAX_VALUE,
+                separator: ','
+            };
+
+            // initialize the chips
+            if(this.target_.value) {
+                // initialize the chips
+                this.target_.value.split(this.options_.separator).forEach(this.addChip_.bind(this));
+            }
+        }
     }
-    // remove button
-    objs.field.on('click', '.mdl-chip__action', function() {
-        $(this).parent().remove();
-    });
-};
-
-
-$(document).ready(function() {
-    $('#demo-0').chips_input({});
-    $('#demo-1').chips_input({});
-    $('#demo-2').chips_input({
-        search: function(query, callback) {
-            var countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Anguilla", "Antigua &amp; Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia &amp; Herzegovina", "Botswana", "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Cape Verde", "Cayman Islands", "Chad", "Chile", "China", "Colombia", "Congo", "Cook Islands", "Costa Rica", "Cote D Ivoire", "Croatia", "Cruise Ship", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Estonia", "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", "France", "French Polynesia", "French West Indies", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", "Guinea", "Guinea Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kuwait", "Kyrgyz Republic", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Mauritania", "Mauritius", "Mexico", "Moldova", "Monaco", "Mongolia", "Montenegro", "Montserrat", "Morocco", "Mozambique", "Namibia", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russia", "Rwanda", "Saint Pierre &amp; Miquelon", "Samoa", "San Marino", "Satellite", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "South Africa", "South Korea", "Spain", "Sri Lanka", "St Kitts &amp; Nevis", "St Lucia", "St Vincent", "St. Lucia", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor L'Este", "Togo", "Tonga", "Trinidad &amp; Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks &amp; Caicos", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "Uruguay", "Uzbekistan", "Venezuela", "Vietnam", "Virgin Islands (US)", "Yemen", "Zambia", "Zimbabwe"];
-            callback($.grep(countries, function(item) {
-                return item.search(new RegExp(query, "i")) != -1;
-            }));
-        }
-    });
-
-
-});
+})();
